c55e8003a213705c982229973ad1416e9be15f08
Semra Bahadir <sb115@students.uwf.edu>
2020-01-12 14:49:35 -0500

    <first commit>

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+        System.out.println("bahadirsemra:BahadirSemra");
+        for (int i = 1; i <=100 ; i++) {
+            System.out.print(" "+i);
+            if (i%5==0) {
+                System.out.println();
+            }
+        }
+    }
+}
