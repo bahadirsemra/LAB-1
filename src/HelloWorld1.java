fdcc4762cebdba28c4b3c2979fa077da79bd6d74
Semra Bahadir <sb115@students.uwf.edu>
2020-01-18 08:29:00 -0500

    Lab1-Hello World

---
+++ src/HelloWorld1.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld1 {
+	public static void main(String[] args) {
+        System.out.println("bahadirsemra:BahadirSemra");
+        for (int i = 1; i <=100 ; i++) {
+            System.out.print(" "+i);
+            if (i%5==0) {
+                System.out.println();
+                System.out.println();
+            }
+        }
+    }
+}
